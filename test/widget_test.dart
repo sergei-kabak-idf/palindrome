0db0dd4bc78c2a52ce4122c3e526d3c436a7ed60
sergei kabak <sergei.kabak@idfinance.com>
2022-02-25 12:32:15 +0300

    fixed controller text

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:palindrome/main.dart';
+import 'package:presentation/app.dart';
 
 
 void main() {
