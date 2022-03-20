c8cf0897af31653ad9aa3c3a886dbc26437f3ea2
sergei kabak <sergei.kabak@idfinance.com>
2022-03-20 11:55:25 +0300

    fixed

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:palindrome/di/injector.dart';
 import 'package:presentation/app.dart';
 
-void main() async {
+Future<void> main() async {
   await configureDependencies();
   runApp(const MyApp());
 }
