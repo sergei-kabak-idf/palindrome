98924a2dbad99368a9312fd4a716762939d59785
sergei kabak <sergei.kabak@idfinance.com>
2022-03-25 14:26:37 +0300

    fixed

---
+++ packages/domain/lib/model/api_palindrome_response.g.dart
@@ -0,0 +1,19 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'api_palindrome_response.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+ApiPalindromeResponse _$ApiPalindromeResponseFromJson(
+        Map<String, dynamic> json) =>
+    ApiPalindromeResponse(
+      json['isPalindrome'] as bool?,
+    );
+
+Map<String, dynamic> _$ApiPalindromeResponseToJson(
+        ApiPalindromeResponse instance) =>
+    <String, dynamic>{
+      'isPalindrome': instance.isPalindrome,
+    };
